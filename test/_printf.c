533442874dac88c2b159ba507a0cd658674571e3
julianfrancor <julianfrancor@gmail.com>
2020-03-15 22:04:35 +0000

    test1

--- test/_printf.c
+++ test/_printf.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
-int _printf(const char *format, ...);
+
+int _printf(const char *format, ...)
+{
+int i = 0;
+va_list valist;
+
+va_start(valist, format);
+/**while (format[i])
+{
+	switch (format[i])
+	{
+		case 'c':
+		putchar((char)va_arg(valist, int));
+		putchar('\n');
+		case "s":
+		//while
+		putchar((char)va_arg(valist, int));
+		putchar('\n');
+	/**case "i":
+	case "d":
+	case "u":
+	case "b":
+	case "o":
+	case "x":
+	case "X":
+	case "p":
+	case "S":
+	case "r":
+	case "R":*/
+	default:
+		break;
+	}
+	//i++;
+//}
+	va_end(valist);
+	return (0);
+}
