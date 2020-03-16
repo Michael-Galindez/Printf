09a3745d2b1344f6662c33d89c70e3f43f488749
julianfrancor <julianfrancor@gmail.com>
2020-03-16 00:11:42 +0000

    adding string printing

--- test/_printf.c
+++ test/_printf.c
@@ -3,15 +3,39 @@
 #include "holberton.h"
 #include <unistd.h>
 
+/**
+ * _puts - function that
+ * prints a string, followed by
+ * a new line, to stdout.
+ * @str: input is a parameter
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != 0)
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar('\n');
+}
+
 int _printf(const char *format, ...)
 {
-int j, i = 0;
-va_list valist;
+int i;
+char *ptr;
 
+va_list valist;
 va_start(valist, format);
+i = 0;
 while (format[i])
 {
-	if (format[i] != '%' && format[i-1] != '%')
+	if (format[i] != '%' && format[i - 1] != '%')
 	{
 		putchar(format[i]);
 	}
@@ -22,10 +46,8 @@ while (format[i])
 			case 'c':
 				putchar((char)va_arg(valist, int));
 			case 's':
-			j = 0;
-			while(format[j])
-				j++;
-			write(1, format, j);
+				ptr = va_arg(valist, char *);
+				write(1, ptr , strlen(ptr));
 			/**putchar((char)va_arg(valist, int));
 				format++;*/
 		
