c2071ae4c709a2f94e2ad0d1848854641e59b08e
julianfrancor <julianfrancor@gmail.com>
2020-03-16 20:39:19 +0000

    fixing the header

--- holberton.h
+++ holberton.h
@@ -1,9 +1,26 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct format_t - format specifier o el tipo marica
+ * @type: format specificadorrrr
+ * @func: output function to be used 
+ */
+typedef struct format_t
+{
+	char *type;
+	int (*func)(va_list);
+} format_t;
+
 int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list valist);
+int print_number(va_list valist);
+int print_reverse(va_list valist);
+int _putchar(char c);
+int _strlen(char *s);
+int (*get_function(const char *s))(va_list valist);
+
 #endif
