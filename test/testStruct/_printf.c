680cb6fadb85e1eac241e84857a61c16280d577b
Michael-Galindez <michaelgalindez274@gmail.com>
2020-03-16 16:05:25 -0500

    Finishing _printf

--- test/testStruct/_printf.c
+++ test/testStruct/_printf.c
@@ -1,48 +1,63 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include "holberton.h"
+#include <stdlib.h>
 #include <unistd.h>
+#include "holberton.h"
 
-
+/**
+ * _printf - prints output according to the format specified.
+ * @format: character string containing format specifiers
+ * Return: The number of chacters printed. Return -1 on error.
+ */
 int _printf(const char *format, ...)
 {
-	int i, count;
-
-	int count, i;
+	int counter, i;
 	va_list valist;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(valist, format);
+
 	count = 0;
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+	   if (format[i] == '%')
 		{
-			/* si encuentra el %%%%%%% */
+			/* si encuentra el %%%% del mudluo*/
 			i++;
 			if (format[i] == '%')
 			{
 				_putchar('%');
-				count++;
+				counter++;
 			}
-	       else if (format[i] == 's' || format[i] == 'd' ||
-	       		  format[i] == 'i' || format[i] == 'c' ||
-			format[i] == 'r' || format[i] == 'x' || format[i] == 'X')
-	       {
-		       count += get_function(&format)(valist);
-	       }
-	       else if (format[i])
-	       {
-		       _putchar('%')
-			       _putchar(format[i])
-			       count + 2;
-				   }
-
-if ()
-
-
-va_end(valist);
-	return (0);
+			else if (format[i] == 's' || format[i] == 'd' ||
+				  format[i] == 'i' || format[i] == 'c' ||
+				 format[i] == 'r')
+			{
+				counter += get_function(&format[i])(valist);
+			}
+			else if (format[i] != '\0')
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				counter += 2;
+			}
+			else if (format[i] == '\0' && (i - 1) == 0)
+			{
+				return (-1);
+			}
+			else
+			{
+				_putchar('%');
+				counter++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			counter++;
+		}
+	}
+	va_end(valist);
+	return (counter);
 }
